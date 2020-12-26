bb897cd113243836bf9f194021762257d08b784a
Jakub Pocentek <jpocentek@gmail.com>
2020-12-26 08:17:03 +0000

    Removed build from repo

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,5 +3,21 @@ import $ from 'jquery';
 
 import '../sass/style.scss';
 
-console.log(PhotoSwipeUI_Default);
-console.log(PhotoSwipe);
+var $pswp = $('.pswp')[0];
+var image = [];
+
+function extractImageData($el) {
+  var $linkEl = $el.find('a').first(),
+    size = $linkEl.data('size').split('x');
+
+  return {
+    src: $linkEl.attr('href'),
+    msrc: $linkEl.find('img').first().attr('src'),
+    w: parseInt(size[0], 10),
+    h: parseInt(size[1], 10)
+  };
+}
+
+$('.gallery').find('figure').each(function() {
+  console.log(extractImageData($(this)));
+});
