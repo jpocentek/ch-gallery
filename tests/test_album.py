fa9cac328f1c4a919f5fc9da512c6275e318baa9
Jakub Pocentek <jpocentek@gmail.com>
2021-01-09 09:17:31 +0000

    Tests for album creation form

--- tests/test_album.py
+++ tests/test_album.py
@@ -7,6 +7,7 @@ from chgallery.db.declarative import Album
 
 __URLCONFIG__ = {
     'album_list': '/album/',
+    'album_create': '/album/create',
     'album_delete': '/album/{album_id}/delete',
     'login_redirect': '/auth/login',
 }
@@ -22,6 +23,45 @@ class TestAlbumListClass:
     def test_authorized_user_can_access_album_list(self, auth, client):
         auth.login()
         assert client.get(__URLCONFIG__['album_list']).status_code == 200
+
+
+class TestAlbumCreateClass:
+
+    def test_anonymous_user_cannot_access_album_create_form(self, client):
+        response = client.get(__URLCONFIG__['album_create'])
+        assert response.status_code == 302
+        assert response.headers['location'].endswith(__URLCONFIG__['login_redirect'])
+
+    def test_authorized_user_can_access_album_create_form(self, auth, client):
+        auth.login()
+        response = client.get(__URLCONFIG__['album_create'])
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize(('name', 'description', 'message'), (
+        ('', '', b'This field is required'),
+        ('xtLCv0FQk.z.e:TnKmm$bI#jWE~y8QyWC', '', b'Field cannot be longer than 32 characters'),
+        ('Album Name', '~qI>h%A7DeLA2J<8;O3yU<lpei=m4fvG%$PT$JeDGH1ItQkOxi9eU7F*7OMW>ei;yKMhr%Rrj4euxPoU:2bqbDWB>Jj6f$waxbPt,lKDlxgSdb0Zypk:w:itCh~yd<.CoG3rsRVeWAjALET%R7E9;v~vr:~oc9ONI.k=n<:am%FFvr.ros%>iwVu.AoDgIDmCY&PR$6UK5BFtZi:zT~dT**63bd9Y=C8L;qUd.JcQekBl*ue=Ky*S0LEXjJAQ2au', b'Field cannot be longer than 255 characters'),  # noqa
+    ))
+    def test_form_validation_with_invalid_data(self, auth, client, name, description, message):
+        auth.login()
+        data = {'name': name, 'description': description}
+        response = client.post(__URLCONFIG__['album_create'], data=data)
+        assert response.status_code == 200
+        assert message in response.data
+
+    def test_successful_album_creation(self, app, auth, client):
+        auth.login()
+        data = {'name': "Test Album 1", 'description': "Test Album Description"}
+        response = client.post(__URLCONFIG__['album_create'], data=data)
+        assert response.status_code == 302
+        assert response.headers['location'].endswith(__URLCONFIG__['album_list'])
+
+        with app.app_context():
+            db_session = get_db_session()
+
+        instance = db_session.query(Album).filter(Album.author_id == 1).first()
+        assert instance.name == "Test Album 1"
+        assert instance.description == "Test Album Description"
 
 
 class TestAlbumDeleteClass:
